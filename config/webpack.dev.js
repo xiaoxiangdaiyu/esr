da0f6479837b867ea21068eb5b29751f63b8c761
xiaoxiangdaiyu <935829048@qq.com>
2018-07-24 19:17:35 +0800

    add

---
+++ config/webpack.dev.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+module.export = {
+    
+}
---
+++ src/utils/render.js
@@ -0,0 +1,5 @@
+import Provider from 'react-redux';
+
+const renders = (store)=>{
+    return 
+}

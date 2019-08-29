43d7669073a3804590bc37d9e94c530757c6a7cc
xiaoxiangdaiyu <935829048@qq.com>
2019-08-29 21:30:33 +0800

    修改文件路径

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,48 +1,35 @@
-var fs = require('fs');
 var path = require('path');
 var glob = require('glob');
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var basePath = path.join(process.cwd(), '/src/pages')
-var files = glob.sync(path.join(basePath + '*.js'))
+var files = glob.sync(path.join(basePath,'*.js'))
 // 入口地址
 var Entrys = {}
 files.forEach(function (file) {
-    var releativePath = path.releative(basePath, file)
-    getEntrys[releativePath.replace(/\.js/, '')] = releativePath
+    var releativePath = path.relative(basePath, file)
+    Entrys[releativePath.replace(/\.js/, '')] = file
 })
-// console.log(files)
-module.export = {
-    entry: Entrys,
-    // target: 'node',
+console.log(path.resolve(__dirname, './src'))
+console.log(path.resolve(__dirname, './src/test.js'))
+module.exports = {
+    mode:'production',
+    entry:{
+        app:'./src/test.js',
+        main:'./src/test.js'
+    },
     output: {
         path: process.cwd() + '/dist',
-        // 直接的入口模块名
-        filename: '[name].js',
-        // 非入口模块，也就是不需要打包到一起的，但又可能会用到，
-        // 这不就是按需加载的么
+        filename: '[name].[hash].js',
         chunkFilename: '[name].[chunkhash].js',
         crossOriginLoading: 'anonymous'
     },
     cache: true,
-    node: {
-        fs:'empty'
-    },
     devtool: 'cheap-source-map',
-    /**
-     * 望文生意，从外部扩展
-     * 防止将某些 import 的包(package)打包到 bundle 中
-     * 支持下面这几种写法  string array object function regex
-     * 即不需要打包进来的
-     * 假如用到了jq
-    */
     externals: {
-        // 此用法表明，将会搜索全局jQuery变量来替代 import $ from 'jquery' 中的jquery
         jquery: 'jQuery'
-        // jquery: {
-
-        // }
     },
     module: {
         rules: [
@@ -51,29 +38,37 @@ module.export = {
                 include: [
                     path.resolve(__dirname, "../src")
                 ],
-                exclude: /node_modules/,
+                exclude: [
+                    'node_modules',
+                    path.resolve(__dirname, "../node_modules")
+                ],
                 use: [
                     {
                         loader: 'babel-loader',
-                        options: {
-                            presets: ['es2015']
-                        }
+                        options:{
+                            presets:['es2015']
+                        } 
                     }
                 ]
             }
         ]
     },
     resolve: {
-        modules: [
-            'node_modules'
-        ],
         extensions: [".js", ".json", ".jsx", ".css"],
     },
+    devServer: {
+        proxy: { // proxy URLs to backend development server
+            '/api': 'http://localhost:3000'
+        },
+        contentBase: path.resolve(__dirname, "../dist"),
+        hot: true,
+        open:true
+    },
     plugins: [
-        // new CleanWebpackPlugin(['dist']),
-        // new HtmlWebpackPlugin({
-        //     title: 'test'
-        // })
-        // new webpack.HotModuleReplacementPlugin()
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title:'test'
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ]
 }
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,23 +3,28 @@ var glob = require('glob');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
-
 var basePath = path.join(process.cwd(), '/src/pages')
 var files = glob.sync(path.join(basePath,'*.js'))
 // 入口地址
-var Entrys = {}
+var Entrys = {},
+    htmls = []
 files.forEach(function (file) {
     var releativePath = path.relative(basePath, file)
-    console.log(file)
-    console.log(__dirname)
-    Entrys[releativePath.replace(/\.js/, '')] = file
+    console.log('file>>>',file)
+    console.log('__dirname',__dirname)
+    var name = releativePath.replace(/\.js/, '').toLowerCase()
+    Entrys[name] = file
+    htmls.push(new HtmlWebpackPlugin({
+        filename: name+ '.html',
+        template:'./src/index.html',
+        chunks: ['common', name]
+    }))
 })
-console.log(Entrys)
-module.export = {
+module.exports = {
     entry: Entrys,
     mode: 'development',
     output: {
-        path: process.cwd() + '/dist',
+        path: path.join(process.cwd(), '/dist'),
         // 直接的入口模块名
         filename: '[name].js',
         // 非入口模块，也就是不需要打包到一起的，但又可能会用到，
@@ -27,8 +32,7 @@ module.export = {
         chunkFilename: '[name].[chunkhash].js',
         crossOriginLoading: 'anonymous'
     },
-    cache: true,
-    devtool: 'cheap-source-map',
+    devtool: 'inline-source-map',
     /**
      * 望文生意，从外部扩展
      * 防止将某些 import 的包(package)打包到 bundle 中
@@ -47,43 +51,35 @@ module.export = {
         rules: [
             {
                 test: /\.(js|jsx)?$/,
-                include: [
-                    path.resolve(__dirname, "../src")
-                ],
-                exclude: [
-                    'node_modules',
-                    path.resolve(__dirname, "../node_modules")
-                ],
+                include: path.join(process.cwd(), "/src"),
+                exclude: path.resolve(process.cwd(), '/node_modules'),
                 use: [
                     {
-                        loader: 'babel-loader',
-                        options:{
-                            presets:['es2015']
-                        } 
+                        loader: 'babel-loader'
                     }
                 ]
             }
         ]
     },
     resolve: {
-        modules: [
-            'node_modules'
-        ],
         extensions: [".js", ".json", ".jsx", ".css"],
     },
     devServer: {
-        proxy: { // proxy URLs to backend development server
-            '/api': 'http://localhost:3000'
-        },
-        contentBase:'./dist',
+        // 指明静态文件例如html应该从何处获取，包括js和css等bundle
+        contentBase: path.join(process.cwd(), '/src'),
         hot: true,
-        open:true
+        open:true,
+        // 不过，如果设置了该属性，则js等静态资源路径需要加上该前缀
+        // 假设服务器运行在 http://localhost:8080 并且 output.filename 被设置为 bundle.js。
+        // 默认 devServer.publicPath 是 '/'，
+        // 所以你的包(bundle)可以通过 http://localhost:8080/bundle.js 访问。
+        // 修改 devServer.publicPath，将 bundle 放在指定目录下：
+        // 上面的例子很清晰，概括就是静态资源bundle输出位置，指定了则在该文件夹下，访问时需要加上该path
+        publicPath:'/'
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title:'test'
-        }),
+        new CleanWebpackPlugin(),
+        ...htmls,
         new webpack.HotModuleReplacementPlugin()
     ]
 }

eb577d68c7661931766385b8b8af45e7a69d1f3a
xiaoxiangdaiyu <935829048@qq.com>
2019-09-16 15:19:22 +0800

    up

--- ssr/server/middlewares/render.js
+++ ssr/server/middlewares/render.js
@@ -1,1 +1,8 @@
 // 区分后端接口与路径
+
+module.exports = async (ctx,next)=>{
+    if(ctx.path.match('.html')){
+
+    }
+    await next()
+}
--- ssr/server/route.js
+++ ssr/server/route.js
@@ -1,8 +1,18 @@
-const home = require('./controls/home');
-
-module.exports = (router,ctx) => {
-    console.log('router>>',router)
-    // router.get('/', home.renderHtml);
-    // router.get('/favicon.ico', home.favicon);
-    // router.get('/test', home.test);
+/**
+ * js逻辑需包含在 extract函数体内
+ * @param {*} extractor 网关执行上下文
+ */
+function extract(extractor) {
+    //  执行并获取 invoke-http-1 的返回内容
+    var getFetchData = function(){
+        var results = extractor.extract('$.tasks.invoke-http-1.output');
+        return JSON.parse(results);
+    };
+    function main(){
+        var originData = getFetchData()
+        var id = originData.id
+        // 裁剪数据，去除冗余数据
+        return JSON.stringify(id)
+    } 
+    return main()
 }
